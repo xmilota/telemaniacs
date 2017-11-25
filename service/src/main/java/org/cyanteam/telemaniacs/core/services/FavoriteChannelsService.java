02b705f1619d384711120cb99c553afd99e02912
Miroslav Kubus <kubus.miro@gmail.com>
2017-11-25 21:16:16 +0100

    #58 - Add favoriteChannelsService interface

---
+++ service/src/main/java/org/cyanteam/telemaniacs/core/services/FavoriteChannelsService.java
@@ -0,0 +1,33 @@
+package org.cyanteam.telemaniacs.core.services;
+
+import java.util.List;
+import org.cyanteam.telemaniacs.core.entities.Channel;
+import org.cyanteam.telemaniacs.core.entities.User;
+
+/**
+ * Interface describes methods for 
+ * work with favorite channels.
+ * @author Miroslav Kubus
+ */
+public interface FavoriteChannelsService {
+    /**
+     * User will start following specific channel
+     * @param channel to be followed
+     * @param user who wants to follow channel
+     */
+    void followChannel(Channel channel, User user);
+    
+    /**
+     * User will stop following specific channel
+     * @param channel to be unfollowed
+     * @param user who wants to unfollow channel 
+     */
+    void unfollowChannel(Channel channel, User user);
+    
+    /**
+     * Method returns all favorite channels of user
+     * @param user whose channel will be returned
+     * @return list of favorite channels
+     */
+    List<Channel> getFavoriteChannels(User user);
+}
