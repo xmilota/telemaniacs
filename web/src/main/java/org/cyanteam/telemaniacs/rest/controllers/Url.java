2d51e0234d0d86f16e1678a594f7c591e9cf449c
Miroslav Kubus <kubus.miro@gmail.com>
2017-12-14 20:02:49 +0100

    #140 Refactor urls

--- web/src/main/java/org/cyanteam/telemaniacs/rest/controllers/Url.java
+++ web/src/main/java/org/cyanteam/telemaniacs/rest/controllers/Url.java
@@ -1,7 +1,9 @@
 package org.cyanteam.telemaniacs.rest.controllers;
 
 public class Url {
-    public static final String baseUrl = "/rest";
+    public static final String BASE_URL = "/rest";
 
-    public static final String channel = baseUrl + "/channel";
+    public static final String CHANNEL = BASE_URL + "/channel";
+    
+    public static final String USER = BASE_URL + "/user";
 }
