bd79d7a801a8871a533396bab4b943c5c1175f38
Miroslav Kubus <kubus.miro@gmail.com>
2017-12-16 13:30:55 +0100

    #150 Fixed tests bugs

--- service/src/test/java/org/cyanteam/telemaniacs/core/facades/TransmissionFacadeImplTest.java
+++ service/src/test/java/org/cyanteam/telemaniacs/core/facades/TransmissionFacadeImplTest.java
@@ -1,90 +1,84 @@
 package org.cyanteam.telemaniacs.core.facades;
 
-import org.cyanteam.telemaniacs.core.ServiceContextConfiguration;
-import org.cyanteam.telemaniacs.core.dto.TransmissionDTO;
-import org.cyanteam.telemaniacs.core.facade.TransmissionFacade;
-import org.junit.After;
+import javax.inject.Inject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
-import static junit.framework.TestCase.assertFalse;
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import org.cyanteam.telemaniacs.core.ServiceContextConfiguration;
+import org.cyanteam.telemaniacs.core.dto.TransmissionDTO;
+import org.cyanteam.telemaniacs.core.entities.Transmission;
+import org.cyanteam.telemaniacs.core.helpers.TransmissionBuilder;
+import org.cyanteam.telemaniacs.core.services.DozerObjectMapperServiceImpl;
+import org.cyanteam.telemaniacs.core.services.ObjectMapperService;
+import org.cyanteam.telemaniacs.core.services.TransmissionService;
+import org.cyanteam.telemaniacs.core.utils.TvManagerDataAccessException;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Simona Tinkova
+ * Refactored by Miroslav Kubus
  */
 @ContextConfiguration(classes = ServiceContextConfiguration.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TransmissionFacadeImplTest {
-    @Autowired
+    
+    @Inject
+    @Spy
+    private final ObjectMapperService mapper = new DozerObjectMapperServiceImpl();
+
+    @Mock
+    private TransmissionService transmissionService;
+    
     @InjectMocks
-    private TransmissionFacade transmissionFacade;
+    private final TransmissionFacadeImpl transmissionFacade = new TransmissionFacadeImpl();
 
-    private TransmissionDTO transmission1;
-    private TransmissionDTO transmission2;
-    private TransmissionDTO newTransmission;
-
+    private Transmission childrenTransmission;
 
     @Before
-    public void createTransmissions() {
-        transmission1 = getTransmissionHelper("tran1");
-        transmission2 = getTransmissionHelper("tran2");
-        newTransmission = getTransmissionHelper("new-transmission");
-
-        transmissionFacade.create(transmission1);
-        assertTrue(transmissionFacade.findAll().contains(transmission1));
-
-        transmissionFacade.create(transmission2);
-        assertTrue(transmissionFacade.findAll().contains(transmission2));
-
-        transmission1 = transmissionFacade.findById(transmission1.getId());
-        transmission2 = transmissionFacade.findById(transmission2.getId());
-    }
-
-
-    @After
-    public void deleteTransmissions() {
-        if (transmissionFacade.findAll().contains(transmission1)) {
-            transmissionFacade.remove(transmission1);
-            assertFalse(transmissionFacade.findAll().contains(transmission1));
-        }
-
-        if (transmissionFacade.findAll().contains(transmission2)) {
-            transmissionFacade.remove(transmission2);
-            assertFalse(transmissionFacade.findAll().contains(transmission2));
-        }
-
-        if (transmissionFacade.findAll().contains(newTransmission)) {
-            transmissionFacade.remove(newTransmission);
-            assertFalse(transmissionFacade.findAll().contains(newTransmission));
-        }
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        
+        childrenTransmission = TransmissionBuilder
+                .sampleIceAgeBuilder()
+                .id(1L)
+                .build();        
+        
+        when(transmissionService.findByName("non-existing"))
+                .thenThrow(new TvManagerDataAccessException());
     }
 
     @Test
     public void shouldCreateTransmission() throws Exception {
-        transmissionFacade.create(newTransmission);
-        assertEquals(transmissionFacade.findById(newTransmission.getId()), newTransmission);
-        assertTrue(transmissionFacade.findAll().contains(newTransmission));
+        TransmissionDTO transmissionDTO = new TransmissionDTO();
+        transmissionDTO.setName("Create transmission");
+        
+        transmissionFacade.create(transmissionDTO);
+        verify(transmissionService).create(any(Transmission.class));
     }
 
     @Test
     public void shouldFindByTransmissionId() throws Exception {
-        assertEquals(transmissionFacade.findById(transmission1.getId()), transmission1);
-        assertEquals(transmissionFacade.findById(transmission2.getId()), transmission2);
+        transmissionFacade.findById(1L);
+        verify(transmissionService, atLeastOnce()).findById(1L);
+        
     }
 
     @Test
     public void shouldFindByTransmissionName() throws Exception {
-        assertEquals(transmissionFacade.findByName(transmission1.getName()), transmission1);
-        assertEquals(transmissionFacade.findByName(transmission2.getName()), transmission2);
+        transmissionFacade.findByName("Football");
+        verify(transmissionService, atLeastOnce()).findByName("Football");
     }
 
     @Test(expected = DataAccessException.class)
@@ -94,31 +88,26 @@ public class TransmissionFacadeImplTest {
 
     @Test
     public void shouldFindAllTransmissions() throws Exception {
-        assertTrue(transmissionFacade.findAll().contains(transmission1));
-        assertTrue(transmissionFacade.findAll().contains(transmission2));
-        assertEquals(transmissionFacade.findAll().size(), 2);
+        transmissionFacade.findAll();
+        verify(transmissionService, atLeastOnce()).findAll();
     }
 
     @Test
     public void shouldUpdateTransmission() throws Exception {
-        transmission1.setName("tran");
-        //logger.info("Transmission id: " + transmission1.getId());
-        transmissionFacade.update(transmission1);
-        assertEquals(transmissionFacade.findAll().size(), 2);
-        assertEquals(transmissionFacade.findById(transmission1.getId()), transmission1);
+        TransmissionDTO transmissionDTO = new TransmissionDTO();
+        transmissionDTO.setName("Update name");
+        transmissionDTO.setId(1L);
+
+        transmissionFacade.update(transmissionDTO);
+        verify(transmissionService).update(any(Transmission.class));
     }
 
     @Test
     public void shouldDeleteTransmission() throws Exception {
-        assertNotNull(transmissionFacade.findById(transmission1.getId()));
-        transmissionFacade.remove(transmission1);
-        assertFalse(transmissionFacade.findAll().contains(transmission1));
-    }
-
-
-    private TransmissionDTO getTransmissionHelper(String name) {
         TransmissionDTO transmissionDTO = new TransmissionDTO();
-        transmissionDTO.setName(name);
-        return transmissionDTO;
+        transmissionDTO.setId(1L);
+        
+        transmissionFacade.remove(transmissionDTO);
+        verify(transmissionService).remove(any(Transmission.class));
     }
 }
