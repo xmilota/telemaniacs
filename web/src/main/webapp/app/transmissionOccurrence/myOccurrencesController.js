4cbe9ef41cd00a85f287d958f511351a3275ef44
tinko <simonatinkova@gmail.com>
2017-12-18 00:10:27 +0100

    occurrence list

--- web/src/main/webapp/app/transmissionOccurrence/myOccurrencesController.js
+++ web/src/main/webapp/app/transmissionOccurrence/myOccurrencesController.js
@@ -10,10 +10,21 @@ telemaniacsApp.controller('TransmissionOccurrencesListController', [
         pageService.setPageName('Occurrences Administration');
 
         pageService.getDataAsync('/transmission/'+ $routeParams.id).then(function (response) {
+            $scope.transmission = response;
             $scope.occurrences = response.occurrences;
             console.log($scope.occurrences);
         });
 
+        $scope.delete = function (occurrence) {
+            console.log('Delete');
+            var errorMessages = {
+                'DataAccessException': 'Occurrence for deletion does not exist!',
+                'otherwise': 'Occurrence cannot be deleted: {msg}'
+            };
+
+            pageService.sendDataAsync('occurrence/' + occurrence.id, 'DELETE', occurrence, 'Occurrence was deleted.',
+                'admin/occurrences/', errorMessages);
+        };
 
     }
 ]);
